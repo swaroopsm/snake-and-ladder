28e4c7a830509d353d149033e8389b03461929ae
Swaroop SM <swaroop@cybrilla.com>
2015-09-12 21:42:46 +0530

    added report for outputing and also increased the board size to 36

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var Board = require('./src/board.js');
 
 // Set the board with total number of cells
-var board = new Board(25);
+var board = new Board(36);
 
 // Add Snakes
 board.addSnake(12, 2);
+board.addSnake(14, 11);
 board.addSnake(17, 4);
+board.addSnake(31, 19);
+board.addSnake(35, 22);
 
 // Add Ladders
-board.addLadder(2, 25);
+board.addLadder(3, 16);
+board.addLadder(5, 7);
+board.addLadder(15, 25);
+board.addLadder(18, 20);
+board.addLadder(21, 32);
 
 // Add Players
 board.addPlayer('Striker')
-     .addPlayer('Vipin')
-     .addPlayer('Stebin')
+     .addPlayer('Matt')
+     .addPlayer('Scott')
      .addPlayer('John')
      .addPlayer('Kathy')
      .addPlayer('Charles');
--- src/board.js
+++ src/board.js
@@ -51,7 +51,7 @@ Board.prototype.addSnake = function(head, tail) {
 };
 
 Board.prototype.addLadder = function(from, to) {
-  this.ladders[from] = new Snake(from, to);
+  this.ladders[from] = new Ladder(from, to);
 };
 
 Board.prototype.addPlayer = function(name) {
@@ -98,7 +98,13 @@ Board.prototype.playAndIsWinner = function() {
 
   cell = this.getCellAt(tempPosition);
 
-  player.paths.push(tempPosition);
+  player.paths.push("["+ diceValue +"]");
+  if(tempPosition <= this.totalCells) {
+    player.paths.push(tempPosition);
+  }
+  else {
+    player.paths.push('X');
+  }
 
   if(cell) {
     // Check if the current cell has a snake
@@ -107,20 +113,26 @@ Board.prototype.playAndIsWinner = function() {
     // Check if the current cell has a ladder
     ladder = this.getLadderAt(tempPosition);
 
+    // Move player to the tail of snake
     if(snake) {
       player.position = snake.tail;
       player.paths.push(snake.tail);
     }
+    
+    // Move player to the top of the ladder
     else if(ladder) {
       player.position = ladder.to;
-      player.paths.push(ladder.to);
+      player.paths.push(player.position);
     }
+
+    // Move player ahead
     else {
       if(tempPosition <= this.totalCells) {
         player.position += diceValue;
       }
     }
-    
+
+    // If player reaches the top, he wins
     if(player.position === this.totalCells) {
       return true;
     }
@@ -134,7 +146,14 @@ Board.prototype.playAndIsWinner = function() {
 };
 
 Board.prototype.reportDetails = function() {
+  var players = this.players;
 
+  for(var i=0, length=players.length; i<length; i++) {
+    console.log(players[i].name, " : ", players[i].paths.join(' -> '));
+  }
+
+  console.log("\n");
+  console.log("--> " + this.currentPlayer.name + " wins!");
 };
 
 module.exports = Board;
